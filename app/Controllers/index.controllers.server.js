be50b358f7181deee1fa43225e854449f4ffd46a
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-05-07 12:06:34 -0400

    pig game removed

--- app/Controllers/index.controllers.server.js
+++ app/Controllers/index.controllers.server.js
@@ -1,33 +1,25 @@
 // Author:Riccardo Reali
 // Date: 28-04-2023
 
+export function displayHomePage(req, res, next) {
+  res.render("index", { title: "PlayJS", page: "home" }); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+}
 
-export function displayHomePage(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS', page:'home'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
+export function displayGamesPage(req, res, next) {
+  res.render("index", { title: "PlayJS Games", page: "games" }); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+}
 
-export function displayGamesPage(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS Games', page:'games'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
+export function displayAboutPage(req, res, next) {
+  res.render("index", { title: "PlayJS About", page: "about" }); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+}
 
-export function displayAboutPage(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS About', page:'about'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
+export function displayGame1Page(req, res, next) {
+  res.render("index", {
+    title: "PlayJS Guess Number",
+    page: "listofgames/guess",
+  }); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+}
 
-export function displayGame1Page(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS Guess Number', page:'listofgames/guess'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
-
-export function displayGame2Page(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS Pig Game', page:'listofgames/pig'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
-
-export function displayGame3Page(req, res, next) 
-{
-    res.render('index', {title : 'PlayJS Snake', page:'listofgames/snake'}); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
-};
+export function displayGame3Page(req, res, next) {
+  res.render("index", { title: "PlayJS Snake", page: "listofgames/snake" }); // RENDERS IN THE EJS TEMPLATE CALLED INDEX
+}
--- app/Routes/index.routes.server.js
+++ app/Routes/index.routes.server.js
@@ -1,21 +1,22 @@
 // Author:Riccardo Reali
 // Date: 15-02-2023
 
-
-import {displayHomePage, displayGamesPage, displayAboutPage, displayGame1Page, displayGame2Page, displayGame3Page} from '../Controllers/index.controllers.server.js'
+import {
+  displayHomePage,
+  displayGamesPage,
+  displayAboutPage,
+  displayGame1Page,
+  displayGame3Page,
+} from "../Controllers/index.controllers.server.js";
 import { Router } from "express";
-
 
 const router = Router(); // I am initializing the router function.
 
 // app.use('/')
-router.get('/', displayHomePage);
-router.get('/games', displayGamesPage);
-router.get('/about', displayAboutPage);
-router.get('/listofgames-guess', displayGame1Page);
-router.get('/listofgames-pig', displayGame2Page);
-router.get('/listofgames-snake', displayGame3Page);
-
-
+router.get("/", displayHomePage);
+router.get("/games", displayGamesPage);
+router.get("/about", displayAboutPage);
+router.get("/listofgames-guess", displayGame1Page);
+router.get("/listofgames-snake", displayGame3Page);
 
 export default router;
